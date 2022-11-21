c4a2ba8a7ffef9b564e2f52ad7c62105308ceeb9
Swann <116310385+Swaannnn@users.noreply.github.com>
2022-11-21 23:22:48 +0100

    Add files via upload

---
+++ load.js
@@ -0,0 +1,9 @@
+const loader = document.querySelector(".loader");
+
+window.addEventListener("load", () => {
+  loader.classList.add("fondu-out");
+});
+
+// setTimeout(() => {
+//   document.querySelector(".loader").getElementsByClassName.display = "none";
+// }, 2240);
