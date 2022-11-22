06dbcd16ec6bf548ea27ab0cf155b7a1f4daa36c
Swann <116310385+Swaannnn@users.noreply.github.com>
2022-11-22 15:04:44 +0100

    Delete load.js

--- load.js
+++
@@ -1,9 +0,0 @@
-const loader = document.querySelector(".loader");
-
-window.addEventListener("load", () => {
-  loader.classList.add("fondu-out");
-});
-
-// setTimeout(() => {
-//   document.querySelector(".loader").getElementsByClassName.display = "none";
-// }, 2240);
