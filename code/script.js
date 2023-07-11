1ce738788a7351934cd171c5fa553ee9c7d12417
Swann <swanngille@gmail.com>
2023-07-11 14:59:59 +0200

    modif page a propos de moi

--- code/script.js
+++ code/script.js
@@ -7,7 +7,6 @@ openHeader.addEventListener("click", () => {
   //   scrollBy(0, -100);
   header.style.transform = "translateY(0px)";
   header.style.transition = "0.8s";
-  //   openHeader.style.display = "none";
   openHeader.style.transform = "translateX(calc(100vw)) translateY(15px)";
   openHeader.style.transition = "0.5s";
 });
@@ -16,5 +15,4 @@ window.addEventListener("scroll", () => {
   header.style.transform = "translateY(-60px)";
   openHeader.style.transform =
     "translateX(calc(100vw - 80px)) translateY(15px)";
-  //   openHeader.style.display = "block";
 });
